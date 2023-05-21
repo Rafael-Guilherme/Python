48d818a3f59624e6fdcabfa3cbd93336bedd538b
Rafael <rafael_guilherme@hotmai.com>
2023-05-20 23:01:29 -0300

    variaveis

---
+++ variaveis/variaveis.py
@@ -0,0 +1,21 @@
+idade = 34
+
+print(idade)
+
+nome = 'Rafael Guilherme'
+
+print(nome)
+
+idade = 56
+altura = 1.72
+nome = 'Rafael'
+estudando = True
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+linguagem = input('Qual é a linguagem que você está estudando?')
+
+print('A linguagem que você está estudando é', linguagem)
